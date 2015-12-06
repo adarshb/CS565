0ccc92c40409b5303a8ef3e8da73eb775a42a5ca
adarshb <Arun@Aruns-MacBook-Pro.local>
2015-12-05 18:15:17 -0600

    Reports

---
+++ CrystalReportsApplication5/CrystalReportsApplication5/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("CrystalReportsApplication5")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("CrystalReportsApplication5")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("c0f2054c-417d-4497-9336-ef111628293b")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
